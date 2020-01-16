5f9838bed31abbba12eda317fc9f970f838fe8f1
Sergey Filatov <sergeyfilatov.j@gmail.com>
2020-01-16 07:20:20 +0400

    feat: insert add close button to login modal

--- src/components/modal-windows/login-modal.tsx
+++ src/components/modal-windows/login-modal.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import { LoginForm } from "../forms/login-form";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500]
+  }
+}));
 
 export const LoginModal: React.FC = () => {
+  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
-
   const handleClickOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
@@ -25,6 +36,13 @@ export const LoginModal: React.FC = () => {
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
       >
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={handleClose}
+        >
+          <CloseIcon />
+        </IconButton>
         <DialogContent>
           <LoginForm />
         </DialogContent>
